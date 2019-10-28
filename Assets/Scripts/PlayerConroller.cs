847201fb59f77743a2a7d27b212d7b12afd5472d
Shubham <40598861+vijit101@users.noreply.github.com>
2019-10-28 17:47:42 +0530

    -Added Basic move mechanics

---
+++ Assets/Scripts/PlayerConroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlayerConroller : MonoBehaviour
+{
+    bool movePlanet;
+    Collider2D PlanetCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlanetCollider = gameObject.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch0 = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch0.position);
+            if (touch0.phase == TouchPhase.Began)
+            {
+                Collider2D collidedWith = Physics2D.OverlapPoint(touchPos);
+                if (PlanetCollider == collidedWith)
+                {
+                    movePlanet = true;
+                }
+            }
+            if (touch0.phase == TouchPhase.Moved)
+            {
+                if (movePlanet)
+                {
+                    transform.position = new Vector2(touchPos.x, touchPos.y);
+                }
+            }
+            if (touch0.phase == TouchPhase.Ended)
+            {
+                movePlanet = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public float minx, maxx, miny, maxy;
+    Vector2 targetPosition;
+    public float speed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = GetRandomPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPos();
+        }
+        
+    }
+
+    Vector2 GetRandomPos()
+    {
+        float x = Random.Range(minx, maxx);
+        float y = Random.Range(miny, maxy);
+        return new Vector2(x,y);
+    }
+}
